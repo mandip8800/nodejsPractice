3452a84b579f716afd1b4171fa785c61e8b1aa53
“mandip8800” <sonypendrive11@gmail.com>
2018-01-12 23:29:57 -0600

    Learning writing to stream

---
+++ writingToAFile.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var data = 'Practice writing to stream and this is the data';
+
+var writeStream = fs.createWriteStream('output1.txt');
+writeStream.write(data,'UTF8');
+writeStream.end();
+writeStream.on('finish',function(){
+	console.log("Writing completed.");
+});
+
+writeStream.on('error',function(){
+	console.log(err.stack);
+});
+
+console.log("Program ended writing.");
