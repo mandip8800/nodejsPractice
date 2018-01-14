c5d3aadd71f00fb8834af71a64a287d765742d74
“mandip8800” <sonypendrive11@gmail.com>
2018-01-13 22:09:32 -0600

    Showing filename with directory.

--- writingToAFile.js
+++ writingToAFile.js
@@ -3,6 +3,7 @@ var data = 'Practice writing to stream and this is the data';
 
 var writeStream = fs.createWriteStream('output1.txt');
 writeStream.write(data,'UTF8');
+
 writeStream.end();
 writeStream.on('finish',function(){
 	console.log("Writing completed.");
